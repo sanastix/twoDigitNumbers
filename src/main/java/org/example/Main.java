cdd552e031f043f723468387cd370c12b99f088b
susanna.stepanian <sanastepanyan@gmail.com>
2023-09-21 20:57:41 +0300

    optimized code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,62 +3,45 @@ package org.example;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
-    public static List<String> list = new ArrayList<>();
-
     public static void main(String[] args) throws FileNotFoundException {
-
         Scanner in = new Scanner(new File("input.txt"));
-
         int a = in.nextInt();
-
         in.close();
 
+        int result = howMuchNumbers(a);
+
         PrintWriter out = new PrintWriter("output.txt");
-        out.print(howMuchNumbers(a));
-
+        out.print(result);
         out.close();
-
     }
 
-    public static int howMuchNumbers (int a){
-        generateStrings(a, "");
-        return list.size();
+    public static int howMuchNumbers(int a) {
+        return generateStrings(a);
     }
 
-    private static void generateStrings(int length, String currentString) {
+    private static int generateStrings(int length) {
+        char[] currentString = new char[length];
+        return generateStringsHelper(length, currentString, 0, ' ');
+    }
 
+    public static int generateStringsHelper(int length, char[] currentString, int index, char lastChar) {
         if (length == 0) {
-            list.add(currentString);
-            return;
+            return 1;
         }
 
-        if (hasThreeConsecutive(currentString + "5")) {
-            generateStrings(length - 1, currentString + "5");
-        }
-        if (hasThreeConsecutive(currentString + "9")) {
-            generateStrings(length - 1, currentString + "9");
-        }
-    }
+        int count = 0;
 
-    public static boolean hasThreeConsecutive(String str) {
-        int n = str.length();
-        if (n < 3) {
-            return true;
-        }
-
-        for (int i = 2; i < n; i++) {
-            if (str.charAt(i) == str.charAt(i - 1) && str.charAt(i) == str.charAt(i - 2)) {
-                return false;
+        for (char digit : "59".toCharArray()) {
+            if (index < 2 || (lastChar != digit || currentString[index - 1] != digit || currentString[index - 2] != digit)) {
+                currentString[index] = digit;
+                count += generateStringsHelper(length - 1, currentString, index + 1, digit);
             }
         }
 
-        return true;
+        return count;
     }
-
 }
